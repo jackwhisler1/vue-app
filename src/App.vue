f227efe9f9558ed37f53f37ce1498be0d33bcd99
jack <whisler1@gmail.com>
2022-03-03 12:31:07 -0600

    follows guide up to emitting events

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,55 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div id="app" class="small-container">
+    <h1>Employees</h1>
+    <employee-form />
+    <employee-table :employees="employees" />
   </div>
 </template>
 
+<script>
+import EmployeeTable from "@/components/EmployeeTable.vue";
+import EmployeeForm from "@/components/EmployeeForm.vue";
+
+export default {
+  name: "app",
+  components: {
+    EmployeeTable,
+    EmployeeForm,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piepiper.com",
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com",
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
 <style>
-#app {
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
+}
+<style > #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
